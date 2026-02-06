d6142a7e67c9b0a7ea9657cce73e43b1001dad91
St1muL <91378088+St1muLFuN@users.noreply.github.com>
2026-02-06 23:34:26 +0100

    Update asset.lua

--- asset.lua
+++ asset.lua
@@ -1,1 +1,1 @@
-"true:RideMotorPart:MainPart:SeatsMotor:CenterSeats:RAM:RP:LAM:LP:HeaderMotorL:UpperPart:HeaderMotorL:UpperPart"
+true:RideMotorPart:MainPart:SeatsMotor:CenterSeats:RAM:RP:LAM:LP:HeaderMotorL:UpperPart:HeaderMotorL:UpperPart
